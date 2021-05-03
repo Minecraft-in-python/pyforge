9f489c21b9e2520db9f04396aa88c0bd20481b22
jason-bowen-zheng <18917572736@wo.cn>
2021-05-03 21:32:18 +0800

    2021-05-03

--- pyforge/command.py
+++ pyforge/command.py
@@ -1,4 +1,13 @@
+import Minecraft.arguments as _arguments
 import Minecraft.command as _command
+
+BaseArgument = _arguments.BaseArgument
+StringArgument = _arguments.StringArgument
+NumberArgument = _arguments.NumberArgument
+BlockArgument = _arguments.BlockArgument
+BooleanArgument = _arguments.BooleanArgument
+PositionArgument = _arguments.PositionArgument
+ArgumentCollection = _arguments.ArgumentCollection
 
 CommandBase = _commands.CommandBase
 
--- pyforge/utils.py
+++ pyforge/utils.py
@@ -11,5 +11,5 @@ PYFORGEVERSION = {
         'major': 0,
         'minor': 3,
         'patch': 1,
-        'str': '0.3.1'
+        'str': '0.3.2'
         }
