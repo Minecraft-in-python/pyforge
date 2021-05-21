c7bea8bc0bba6a32f97493bba39a1357571c5dc4
jason-bowen-zheng <18917572736@wo.cn>
2021-05-21 19:21:33 +0800

    2021-05-21

--- pyforge/manager.py
+++ pyforge/manager.py
@@ -1,21 +1,44 @@
+import sys
+
 from pyforge.command import *
+from pyforge.gui.widget.button import Button
 from pyforge.utils import *
 
 
 class PyforgeManager(CommandBase):
-    formats = [ArgumentCollection(cmd=StringArgument('^(list|version)$'))]
+    formats = [ArgumentCollection(cmd=StringArgument('^(args|list|include-path|version)$'))]
     description = ['pyforge manager',
-            '/pyforge list'
+            '/pyforge args',
+            '/pyforge list',
+            '/pyforge include-path',
             '/pyforge version']
 
     def execute(self):
-        if self.args['cmd'] == 'list':
+        if self.args['cmd'] == 'args':
+            get_minecraft().dialogue.add_dialogue(' '.join(sys.argv))
+        elif self.args['cmd'] == 'list':
             text = '%d mod(s) loaded:\n' % len(mods)
             for mod in mods:
                 text += ' - %s\n' % mod
+            get_minecraft().dialogue.add_dialogue(text)
+        elif self.args['cmd'] == 'include-path':
+            text = 'Values in sys.path:\n'
+            for d in sys.path:
+                text += ' - %s\n' % d
             get_minecraft().dialogue.add_dialogue(text)
         elif self.args['cmd'] == 'version':
             get_minecraft().dialogue.add_dialogue('pyforge %s' % PYFORGEVERSION['str'])
 
 
 register_command('pyforge', PyforgeManager)
+
+def add_widget():
+    mod_button = Button((get_size()[0] - 200) / 2, 210, 200, 40, 'Mods')
+    get_minecraft().guis['pause'].frame.add_widget(mod_button)
+    get_minecraft().register_event('resize', on_resize)
+
+def on_resize(width, height):
+    mod_button.x = (width - 200) / 2
+    mod_button.y = 210
+
+get_minecraft().register_event('init', add_widget)
--- pyforge/utils.py
+++ pyforge/utils.py
@@ -1,9 +1,19 @@
 import Minecraft.utils.utils as _utils
 
+from pyglet.window import key as _keys
+
 get_minecraft = _utils.get_game
 get_size = _utils.get_size
 log_err, log_info, log_warn = _utils.log_err, _utils.log_info, _utils.log_warn
 search_mcpy = _utils.search_mcpy
+
+def get_key(key):
+    if hasattr(_keys, key.upper()):
+        return getattr(_keys, key.upper())
+    elif hasattr(_keys, '_' + key):
+        return getattr(_keys, '_' + key)
+    else:
+        raise KeyError("'%s'" % key)
 
 mods = dict()
 
