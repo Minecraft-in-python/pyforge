4099e8255c7bc96fc57a452802813d336f2ee5d5
jason-bowen-zheng <18917572736@wo.cn>
2021-05-22 19:29:36 +0800

    2021-05-22

--- pyforge/manager.py
+++ pyforge/manager.py
@@ -5,7 +5,7 @@ from pyforge.gui.widget.button import Button
 from pyforge.utils import *
 
 
-class PyforgeManager(CommandBase):
+class pyforgeManager(CommandBase):
     formats = [ArgumentCollection(cmd=StringArgument('^(args|list|include-path|version)$'))]
     description = [assets.get_translation('pyforge.manager.text')[0],
             '/pyforge args',
@@ -14,18 +14,17 @@ class PyforgeManager(CommandBase):
             '/pyforge version']
 
     def execute(self):
+        global mods
         if self.args['cmd'] == 'args':
             get_minecraft().dialogue.add_dialogue(' '.join(sys.argv))
         elif self.args['cmd'] == 'list':
             text = assets.get_translation('pyforge.manager.text')[1] % len(mods)
-            for mod in mods:
-                text += ' - %s\n' % mod
+            text += '\n'.join([' - ' + mod for mod in mods])
             get_minecraft().dialogue.add_dialogue(text)
         elif self.args['cmd'] == 'include-path':
             text = assets.get_translation('pyforge.manager.text')[2]
-            for d in sys.path:
-                text += ' - %s\n' % d
-            get_minecraft().dialogue.add_dialogue(text)
+            text += '\n'.join([' - ' + value for value in sys.path])
+            get_minecraft().dialogue.add_dialogue(text) 
         elif self.args['cmd'] == 'version':
             get_minecraft().dialogue.add_dialogue('pyforge %s' % PYFORGEVERSION['str'])
 
@@ -35,7 +34,7 @@ mod_settings = None
 
 def add_widget():
     global mod_settings
-    mod_settings = Button((get_size()[0] - 200) / 2, 210, 200, 40, assets.get_translation('pyforge.pause_menu.mod_settings'))
+    mod_settings = Button((get_size()[0] - 200) / 2, 210, 200, 40, assets.get_translation('pyforge.menu.mod_settings'))
     get_minecraft().guis['pause'].frame.add_widget(mod_settings)
     get_minecraft().register_event('resize', on_resize)
 
@@ -44,5 +43,5 @@ def on_resize(width, height):
     mod_settings.x = (width - 200) / 2
     mod_settings.y = 210
 
-register_command('pyforge', PyforgeManager)
+register_command('pyforge', pyforgeManager)
 get_minecraft().register_event('init', add_widget)
